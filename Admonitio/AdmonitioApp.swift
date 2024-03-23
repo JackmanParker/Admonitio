d7141eca2115be1606aa0d784cb7e66df674f46f
JackmanParker <113531688+JackmanParker@users.noreply.github.com>
2024-03-23 16:43:00 -0600

    Initial Commit

---
+++ Admonitio/AdmonitioApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdmonitioApp.swift
+//  Admonitio
+//
+//  Created by Parker Jackman on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct AdmonitioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
